dd014e5eaac5a0e2c52a3ca08823b4280ebe56a1
abubakarafridi <abubakarafridi407@gmail.com>
2024-06-01 12:38:38 +0500

    restored

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
   currentFolder = folder;
-  let a = await fetch(`https://cors-anywhere.herokuapp.com/${folder}/`);
+  let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
   let response = await a.text();
   let div = document.createElement('div');
   div.innerHTML = response;
@@ -73,7 +73,7 @@ let playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums() {
-  let a = await fetch(`https://cors-anywhere.herokuapp.com/https://github.com/abubakarafridi/Spotify-clone-project/tree/main/songs`);
+  let a = await fetch(`http://127.0.0.1:5500/songs/`);
   let response = await a.text();
   let div = document.createElement('div');
   div.innerHTML = response;
@@ -84,7 +84,7 @@ async function displayAlbums() {
     const e = array[index];
     if (e.href.includes('/songs/')) {
       let folder = e.href.split('/').slice(-1)[0];
-      let a = await fetch(`https://cors-anywhere.herokuapp.com/https://github.com/abubakarafridi/Spotify-clone-project/tree/main/songs/${folder}/info.json`);
+      let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
       let response = await a.json();
       cardContainer.innerHTML =
         cardContainer.innerHTML +
@@ -101,14 +101,14 @@ async function displayAlbums() {
 
   Array.from(document.getElementsByClassName('card')).forEach((e) => {
     e.addEventListener('click', async (item) => {
-      songs = await getSongs(`https://cors-anywhere.herokuapp.com/https://github.com/abubakarafridi/Spotify-clone-project/tree/main/songs/${item.currentTarget.dataset.folder}`);
+      songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`);
       playMusic(songs[0]);
     });
   });
 }
 
 async function main() {
-  await getSongs('https://cors-anywhere.herokuapp.com/https://github.com/abubakarafridi/Spotify-clone-project/tree/main/songs/ncs');
+  await getSongs('songs/ncs');
   playMusic(songs[0], true);
 
   displayAlbums();
