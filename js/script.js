5d8ad3ae803946ce8f89c4fef21c1ae9bd0cea1a
abubakarafridi <abubakarafridi407@gmail.com>
2024-06-01 12:05:20 +0500

    restored

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
   currentFolder = folder;
-  let a = await fetch(`.${folder}/`);
+  let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
   let response = await a.text();
   let div = document.createElement('div');
   div.innerHTML = response;
@@ -73,7 +73,7 @@ let playMusic = (track, pause = false) => {
 };
 
 async function displayAlbums() {
-  let a = await fetch(`.songs/`);
+  let a = await fetch(`http://127.0.0.1:5500/songs/`);
   let response = await a.text();
   let div = document.createElement('div');
   div.innerHTML = response;
@@ -84,7 +84,7 @@ async function displayAlbums() {
     const e = array[index];
     if (e.href.includes('/songs/')) {
       let folder = e.href.split('/').slice(-1)[0];
-      let a = await fetch(`.songs/${folder}/info.json`);
+      let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
       let response = await a.json();
       cardContainer.innerHTML =
         cardContainer.innerHTML +
